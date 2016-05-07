5f78b192c3bb2a3d657559be5894a36293f595f0
John Qiao <qiaoshun8888@gmail.com>
2016-05-07 02:13:20 -0700

    version2

--- statefarm/src_py/architectures.py
+++ statefarm/src_py/architectures.py
@@ -4,29 +4,29 @@ from layers import Dense, Conv2D, Flatten, Conv2DBatchNorm, AvgPool, Dropout, Ac
 
 def vgg_bn():
     return [
-        Conv2D([3, 3], 32, [1, 1, 1, 1], padding='SAME'),
+        Conv2D([5, 5], 32, [1, 1, 1, 1]),
         Conv2DBatchNorm(32),
         Activation(tf.nn.relu),
 
-        Conv2D([3, 3], 32, [1, 1, 1, 1], padding='SAME'),
+        Conv2D([5, 5], 32, [1, 1, 1, 1]),
         Conv2DBatchNorm(32),
         Activation(tf.nn.relu),
 
         MaxPool([1, 3, 3, 1], [1, 2, 2, 1]),
 
-        Conv2D([3, 3], 64, [1, 2, 2, 1]),
+        Conv2D([3, 3], 64, [1, 1, 1, 1]),
         Conv2DBatchNorm(64),
         Activation(tf.nn.relu),
 
-        Conv2D([3, 3], 64, [1, 1, 1, 1], padding='SAME'),
+        Conv2D([3, 3], 64, [1, 1, 1, 1]),
         Conv2DBatchNorm(64),
         Activation(tf.nn.relu),
 
         MaxPool([1, 3, 3, 1], [1, 2, 2, 1]),
 
-        # Conv2D([3, 3], 128, [1, 2, 2, 1]),
-        # Conv2DBatchNorm(128),
-        # Activation(tf.nn.relu),
+        Conv2D([1, 1], 128, [1, 1, 1, 1]),
+        Conv2DBatchNorm(128),
+        Activation(tf.nn.relu),
 
         # Conv2D([3, 3], 128, [1, 1, 1, 1], padding='SAME'),
         # Conv2DBatchNorm(128),
