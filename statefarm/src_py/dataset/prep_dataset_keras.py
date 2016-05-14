a611cc4002a3545432d7102c3db8b8f6dae439b8
John Qiao <qiaoshun8888@gmail.com>
2016-05-13 20:46:07 -0700

    traning data on the fly - fix5

--- statefarm/src_py/dataset/prep_dataset_keras.py
+++ statefarm/src_py/dataset/prep_dataset_keras.py
@@ -46,11 +46,11 @@ def normalize(img):
     return img
 
 
-def get_driver_data(base):
+def get_driver_data():
     drivers = dict()
     classes = dict()
     print('Read drivers data')
-    f = open(os.path.join(base if base else 'dataset', 'driver_imgs_list.csv'), 'r')
+    f = open('/Users/sqiao/Desktop/kaggle/statefarm/src_py/dataset/driver_imgs_list.csv', 'r')
     line = f.readline()
     while (1):
         line = f.readline()
@@ -70,8 +70,7 @@ def load_train(base):
     X_train_id = []
     y_train = []
     driver_ids = []
-    driver_data_base = os.path.dirname(os.path.dirname(os.path.dirname(base)))
-    driver_data, driver_class = get_driver_data(driver_data_base)
+    driver_data, driver_class = get_driver_data()
     start_time = time.time()
 
     print('Reading train images...')
